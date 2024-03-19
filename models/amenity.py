66a490870f595e9fad09bc594b8058f8d0e1066d
mohamedRajab0 <elzimmohamed@gmail.com>
2024-03-19 05:49:42 +0200

    initial structure

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Amenity class that implements the BaseModel"""
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+
+    """
+    The Amenity class that inherits from BaseModel
+
+    Public class attributes:
+        name : string - empty string
+    """
+
+    name = ""
