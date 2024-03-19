9f3c5940c11d31b4a9a29f0ad131a72a2289f565
mohamedRajab0 <elzimmohamed@gmail.com>
2024-03-19 06:02:44 +0200

    initial structure

---
+++ models/review.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Review class that implements the BaseModel"""
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+
+    """
+    The Review class that inherits from BaseModel
+
+    Public class attributes:
+        place_id: string - empty string: it will be the Place.id
+        user_id: string - empty string: it will be the User.id
+        text: string - empty string
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
