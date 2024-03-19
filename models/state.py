fd7237bd265674a9ff983a05e8f84d913cbe548b
mohamedRajab0 <elzimmohamed@gmail.com>
2024-03-19 05:40:53 +0200

    State Module

---
+++ models/state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""State class that implements the BaseModel"""
+from base_model import BaseModel
+
+
+class State(BaseModel):
+
+    """
+    The State class that inherits from BaseModel
+
+    Public class attributes:
+        name : string - empty string
+        
+    """
+
+    name = ""
