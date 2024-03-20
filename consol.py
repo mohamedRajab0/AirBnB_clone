05e1d0ee853e10f67d9c33869501e66371a559fe
abdallhfattah <abdallh.fattah33@gmail.com>
2024-03-20 15:33:04 +0200

    desotry show consol feature added

--- consol.py
+++ consol.py
@@ -1,9 +1,11 @@
 import cmd
 import json
 
-from models.base_model import  BaseModel
+from models import storage
+from models.base_model import BaseModel
 
 '''The implementation of the console (CLI) for the AirBnB project'''
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -20,40 +22,83 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """Creates a new instance of BaseModel"""
         items = args.split()
-        print(type(items))
-        if len(args) < 1 :
-            print("class name missing")
+        if len(args) < 1:
+            print("** class name missing **")
+            return
 
         for item in items:
             if item != 'BaseModel':
-                print("class doesn't exist")
+                print("** class doesn't exist **")
             else:
                 new_instance = BaseModel()
                 new_instance.save()
                 print(new_instance.__dict__['id'])
+
     def do_show(self, args):
+        """show objects with an id and class name"""
         items = args.split()
         if len(items) < 1:
-            print("class name missing")
+            print("** class name missing **")
         else:
-            name = items[0]
-            inst_id = items[1]
+
+            if len(items) < 2:
+                print("** instance id missing **")
+                return
+
+            name, inst_id = items[0], items[1]
             name_id = name + '.' + inst_id
             if name != 'BaseModel':
-                print('class doesn't exist)
-            with open('file.json' , 'r', encoding='utf-8') as f:
-                my_obj = json.load(f)
-            print(my_obj[name_id])
-            print(len(items))
+                print("** class doesn't exist **")
+                return
+            try:
+                with open('file.json', 'r', encoding='utf-8') as f:
+                    my_obj = json.load(f)
+                if name_id in my_obj:
+                    print(my_obj[name_id])
+                else:
+                    print("** no instance found ** ")
+            except FileNotFoundError:
+                print("File not found error occurred")
+            except json.JSONDecodeError:
+                print("JSON decoding error occurred")
+
+    def do_destory(self, args):
+        """destory objects with an id and class name"""
+        items = args.split()
+        if len(items) < 1:
+            print("** class name missing **")
+        else:
+
+            if len(items) < 2:
+                print("** instance id missing **")
+                return
+
+            name, inst_id = items[0], items[1]
+            name_id = name + '.' + inst_id
+            if name != 'BaseModel':
+                print("** class doesn't exist **")
+                return
+            try:
+                my_obj = storage.all()
+                if name_id in my_obj:
+                    del my_obj[name_id]
+                    storage.save()
+                    return
+
+                print("** no instance found **")
+            except FileNotFoundError:
+                print("File not found error occurred")
+            except json.JSONDecodeError:
+                print("JSON decoding error occurred")
+
+    def do_all(self , args):
+        items = args.split(' ')
+        if len(items) < 1:
+            print("** class doesn't exist **")
 
 
 
-
-
-
-    do_EOF =do_quit
-
-
+    do_EOF = do_quit
 
 
 if __name__ == '__main__':
