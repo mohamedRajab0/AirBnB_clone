f547894977c6616ce708ae308f80c147913b1766
abdallhfattah <abdallh.fattah33@gmail.com>
2024-03-20 12:35:57 +0200

    saving object before writing into file.json

--- models/base_model.py
+++ models/base_model.py
@@ -42,14 +42,16 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # Saving the new object into storage
-            models.storage.new(self)
+            # models.storage.new(self)
+
 
     def save(self):
         """Updates `updated_at` with the current datetime"""
         self.updated_at = datetime.now()
-        # hitting the change to be in storage
+        # Saving the new object into storage
+        models.storage.new(self)
         models.storage.save()
+
 
     def to_dict(self):
         """
