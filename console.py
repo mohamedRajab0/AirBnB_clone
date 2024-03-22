24fc0ec4fbf607c66597de0b0b2557f3ea675ed5
abdallhfattah <abdallh.fattah33@gmail.com>
2024-03-22 15:51:14 +0200

    <class_name>.fun() supported

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 import cmd
 import json
-import sys
+import re
 
 from models import storage
 from models.amenity import Amenity
@@ -20,36 +20,70 @@ class HBNBCommand(cmd.Cmd):
                   'Place': Place, 'City': City, 'Amenity': Amenity}
     prompt = "(hbnb) "
 
+    splitted = False
+
+    items = []
     supported_commands = ['quit', 'create', 'show', 'destory', 'all', 'update']
 
-    # TODO:
-    # def precmd(self, line: str) -> str:
-    # vaildate = line.split(' ')
-    # print(vaildate)
-    # if vaildate[0] in self.supported_commands:
-    #     print("iam actually here")
-    #     print(line)
-    #     return line
-    # # <class name> functionName ("" , " " , " ")
-    # # <class name> functionName ("" , {" " , " " , " "})
-    # new_command = line.split('.')
-    # class_name = new_command[0]
-    # fun_name_with_args = new_command[1]
-    # # print(class_name)
-    # # print(fun_name_with_args)
-    # return self.parse(fun_name_with_args) + ' ' +class_name
+    def precmd(self, line: str) -> str:
+        self.splitted = False
+        self.items = []
+        check = line.split()
 
-    # #TODO:
-    # def parse(self, fun_with_args) -> list:
-    #     command_args = []
-    #     command = fun_with_args.split('(')
-    #     command_args.append(command[0])
-    #     args = command[1].split(',')
-    #     command_args.append(args)
-    #     command_args[-1][-1].replace(")" , "")
-    #     # print(command_args)
+        if check[0] in self.supported_commands:
+            return line
 
-    #     return command[0]
+        try:
+            function_name, args = self.fetch_parts(line)
+            class_name, command = function_name.split('.')
+            args = args.split(',', 1)
+            final_command = f'{command}'
+            self.items.append(class_name)
+            arguments = ""
+            for arg in args:
+                arg = self.parse_str(arg.strip())
+                arguments += (arg + ' ')
+            arguments = arguments[:len(arguments)-1]
+            real_args = self.parse_string_to_list(arguments)
+            # print(real_args)
+            for arg in real_args: self.items.append(arg)
+
+            # print(final_command)
+            # print(self.items)
+            self.splitted = True
+            return final_command
+        except:
+            return line
+
+    def parse_string_to_list(self, input_string):
+        # Use regular expression to find all matches of quoted strings and numbers
+        matches = re.findall(r'"([^"]+)"|(\d+)', input_string)
+        # Flatten the list of tuples and filter out None values
+        result = [item for sublist in matches for item in sublist if item]
+        # print("result : ", result)
+        return result
+
+    def fetch_parts(self, input):
+        match = re.match(r'(\w+\.\w+)\((.*)\)', input)
+        if not match:
+            raise ValueError("Invalid function call format")
+
+        function_name = match.group(1)
+        arguments = match.group(2)
+
+        return function_name, arguments
+
+    def parse_str(self, mystr):
+        my_dict = {"{": 1, "}": 2,
+                   ")": 3, "(": 4, ",": 5, ":": 6}
+        new_str = ""
+        for i in mystr:
+            if i in my_dict.keys():
+                continue
+            if i == '\'':
+                i = '"'
+            new_str += f"{i}"
+        return new_str
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -62,34 +96,35 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """Creates a new instance of BaseModel"""
-
-        items = args.split()
+        if not self.splitted:
+            self.items = args.split()
         if len(args) < 1:
             print("** class name missing **")
             return
 
-        for item in items:
+        for item in self.items:
             if item not in self.my_classes.keys():
                 print("** class doesn't exist **")
             else:
 
-                new_instance = self.my_classes[items[0]]
+                new_instance = self.my_classes[self.items[0]]
                 obj = new_instance()
                 obj.save()
                 print(obj.id)
 
     def do_show(self, args):
         """show objects with an id and class name"""
-        items = args.split()
-        if len(items) < 1:
+        if not self.splitted:
+            self.items = args.split()
+        if len(self.items) < 1:
             print("** class name missing **")
         else:
 
-            if len(items) < 2:
+            if len(self.items) < 2:
                 print("** instance id missing **")
                 return
 
-            name, inst_id = items[0], items[1]
+            name, inst_id = self.items[0], self.items[1]
             name_id = name + '.' + inst_id
             if name not in self.my_classes.keys():
                 print("** class doesn't exist **")
@@ -108,16 +143,17 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destory(self, args):
         """destory objects with an id and class name"""
-        items = args.split(' ')
-        if len(items) < 1:
+        if not self.splitted:
+            self.items = args.split()
+        if len(self.items) < 1:
             print("** class name missing **")
         else:
 
-            if len(items) < 2:
+            if len(self.items) < 2:
                 print("** instance id missing **")
                 return
 
-            name, inst_id = items[0], items[1]
+            name, inst_id = self.items[0], self.items[1]
             name_id = name + '.' + inst_id
             if name not in self.my_classes.keys():
                 print("** class doesn't exist **")
@@ -137,15 +173,15 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Prints all string representation of all instances based or not on the class name"""
-        items = args.split()
+        if not self.splitted:
+            self.items = args.split()
         objects = []
-        num_of_instances = 0
-        # len(items) => 0,1   class => not found
-        if len(items) <= 1 or items[0] in self.my_classes.keys():
+        # print(len(items)) # => 0,1   class => not found
+        if len(self.items) <= 1 or self.items[0] in self.my_classes.keys():
             for instance in storage.all().values():
-                if len(items) != 0 and instance.__class__.__name__ == items[0]:
+                if len(self.items) != 0 and instance.__class__.__name__ == self.items[0]:
                     objects.append(instance.__str__())
-                elif len(items) == 0:
+                elif len(self.items) == 0:
                     objects.append(instance.__str__())
             print(objects)
         else:
@@ -153,18 +189,17 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, args):
         """ Updates an instance based on the class name and id by adding or updating attribute """
-        items = args.split()
-        for item in items:
-            print(item)
-        if len(items) < 1:
+        if not self.splitted:
+            self.items = args.split()
+        if len(self.items) < 1:
             print("** class name missing **")
         else:
 
-            if len(items) < 2:
+            if len(self.items) < 2:
                 print("** instance id missing **")
                 return
 
-            name, inst_id = items[0], items[1]
+            name, inst_id = self.items[0], self.items[1]
             name_id = name + '.' + inst_id
             if name not in self.my_classes.keys():
                 print("** class doesn't exist **")
@@ -175,17 +210,18 @@ class HBNBCommand(cmd.Cmd):
                 if name_id not in my_object:
                     print("** no instance found ** ")
                     return
-                if len(items) < 3:
+                if len(self.items) < 3:
                     print("** attribute name missing **")
                     return
-                if len(items) < 4:
+                if len(self.items) < 4:
                     print("** value missing **")
                     return
                 # <atrr> value <attr> value
                 #         0                 1                       2    3      4          5
                 # update User f7a9c035-8d67-4867-ab57-cf77277411c3 name ypuss my_number 219191
-                for i in range(2, len(items) - 1, 2):
-                    setattr(my_object[name_id], items[i],  items[i+1])
+                for i in range(2, len(self.items) - 1, 2):
+                    setattr(my_object[name_id],
+                            self.items[i],  self.items[i+1])
                 storage.save()
             except FileNotFoundError:
                 print("File not found error occurred")
@@ -193,13 +229,13 @@ class HBNBCommand(cmd.Cmd):
                 print("JSON decoding error occurred")
 
     def do_count(self, args):
-        items = args.split()
-        if items[0] not in self.my_classes.keys():
+        self.items = args.split()
+        if self.items[0] not in self.my_classes.keys():
             print("** class doesn't exist **")
             return
         obj_conuting = 0
         for instance in storage.all().values():
-            if instance.__class__.__name__ == items[0]:
+            if instance.__class__.__name__ == self.items[0]:
                 obj_conuting += 1
         print(obj_conuting)
 
@@ -208,14 +244,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
-
-
-#TODO:
-    # <class name>.Function( {})   => Function + <class name> + ()
-    # "asojj oisajdf" => " "
-
-    # User.update("38f22813-2753-4d42-b37c-57a17f1e4f88", {'first_name': "John", "age": 89}) 
-    # update User 38f22813-2753-4d42-b37c-57a17f1e4f88 first_name Jhon age 89
-    # User.update("38f22813-2753-4d42-b37c-57a17f1e4f88", "first_name", "John")
-    # update User 38f22813-2753-4d42-b37c-57a17f1e4f88 first_name Jhon
