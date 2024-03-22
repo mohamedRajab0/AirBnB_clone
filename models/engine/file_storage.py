6973af74ef3f26f70de020ec80c52d44fd599946
abdallhfattah <abdallh.fattah33@gmail.com>
2024-03-22 13:27:03 +0200

    loading from file as an objects in reload function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,23 +20,56 @@ class FileStorage:
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path) """
+        dict_objs = {}
+        for key, obj in FileStorage.__objects.items():
+            dict_objs[key] = obj.to_dict()
         with open(self.__file_path, 'w', encoding="utf-8") as f:
-            json.dump(FileStorage.__objects, f)
+            json.dump(dict_objs, f)
+
 
     def reload(self):
-        """  
+        """
           deserializes the JSON file to __objects
           (only if the JSON file (__file_path) exists ; otherwise, do nothing.
           If the file doesn't exist, no exception should be raised)
           """
-    
+
         try:
             with open(FileStorage.__file_path, 'r') as f:
-                FileStorage.__objects = json.load(f)
+                dict_objs = json.load(f)
+
+
+            from models.amenity import Amenity
+            from models.base_model import BaseModel
+            from models.city import City
+            from models.place import Place
+            from models.review import Review
+            from models.state import State
+            from models.user import User
+
+            classes = {
+                'BaseModel': BaseModel,
+                'User': User,
+                'State': State,
+                'City': City,
+                'Place': Place,
+                'Amenity': Amenity,
+                'Review': Review
+            }
+
+            for key, obj_dict in dict_objs.items():
+                cls_name, inst_id = key.split('.')
+                # fetching class name
+                cls = classes[cls_name]
+                # creating an object from dictionary
+                new_obj = cls(**obj_dict)
+                # loading classes instances into __objects as dictionary
+                self.new(new_obj)
+
         except FileNotFoundError:
             return
         except json.JSONDecodeError:
